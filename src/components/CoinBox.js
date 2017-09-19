67b9b63e856af9d58a87bc9a36b6d5ea8615d53f
tony <tbenny001@gmail.com>
2017-09-18 23:53:03 -0400

    CoinBox component CSS changes

--- src/components/CoinBox.js
+++ src/components/CoinBox.js
@@ -6,22 +6,20 @@ export default class CoinBox extends React.Component {
     return (
       <div className="coin-box">
         <p>{coinData.name}({coinData.symbol})</p>
-        <div className="info-box">
-          <div>
-            <p>Price($):</p>
-            <p>7 day % change:</p>
-            <p>24 hour % change:</p>
-            <p>1 hour % change:</p>
-          </div>
-          <div>
-            <p>{coinData.price_usd}</p>
-            <p>{coinData.percent_change_7d}</p>
-            <p>{coinData.percent_change_24h}</p>
-            <p>{coinData.percent_change_1h}</p>
-          </div>
+        <div>
+          <p>Price($):</p>
+          <p>7 day % change:</p>
+          <p>24 hour % change:</p>
+          <p>1 hour % change:</p>
         </div>
-
+        <div>
+          <p>{coinData.price_usd}</p>
+          <p>{coinData.percent_change_7d}</p>
+          <p>{coinData.percent_change_24h}</p>
+          <p>{coinData.percent_change_1h}</p>
+        </div>
       </div>
+
     )
   }
 }
